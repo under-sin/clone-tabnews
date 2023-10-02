cfea16abf9edef9c91eb737d0953494d961a259e
Anderson <61955074+under-sin@users.noreply.github.com>
2023-10-02 00:34:02 +0000

    adiciona script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto fullstask with js
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Liberdade é fazer aquilo que não quer - trouxa</h1>
+  return <h1>Liberdade é fazer aquilo que não quer - trouxa</h1>;
 }
 
-export default Home
+function Test() {
+  console.log("teste");
+}
+
+export default Home;
